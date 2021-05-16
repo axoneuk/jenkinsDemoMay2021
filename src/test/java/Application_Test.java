3cc994b873113ac437c727020fd2acdde33d9780
axoneuk <info@axone-tech.uk>
2021-05-16 08:21:10 +0100

    Commit project to repo

---
+++ src/test/java/Application_Test.java
@@ -0,0 +1,21 @@
+
+import org.junit.jupiter.api.Test;
+
+public class Application_Test {
+
+    @Test
+    public void test1(){
+        System.out.println("running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("running test 3");
+    }
+
+}
